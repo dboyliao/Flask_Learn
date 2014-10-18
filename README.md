524a14220be2814f598c4021756701be74e9df8f
dboyliao <qmalliao@gmail.com>
2014-10-18 11:13:42 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+#Files
+
+- app.py: file where all the python code lives.
+
+
+Dirs:
+
+- static: directory which holds all the static files such as css, js, img, ...etc
+- template: directory which holds all the html files.
---
+++ app.py
@@ -0,0 +1,59 @@
+# g is a object in flask used for storing temp data. Its value is reset every request.
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from functools import wraps
+from flask.ext.sqlalchemy import SQLAlchemy
+import sqlite3
+
+# __name__: the name of current namespace.
+app = Flask(__name__)
+
+app.secret_key = "Dboy"
+app.database = "sample.db"
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You have to login first.")
+            return redirect(url_for("login"))
+    return wrap
+
+@app.route("/")
+@login_required
+def home():
+    g.db = connect_db()
+    cur = g.db.execute("select * from posts")
+    posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("index_block.html", posts = posts)
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome_block.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid credentials. Please check the username or password."
+        else:
+            session["logged_in"] = True
+            flash("You were just logged in.")
+            return redirect(url_for("home"))
+    return render_template("login_block.html", error = error)
+
+@app.route("/logout")
+@login_required
+def logout():
+    session.pop("logged_in", None)
+    flash("You were just logged out.")
+    return redirect(url_for("welcome"))
+
+def connect_db():
+    return sqlite3.connect(app.database)
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8888)
